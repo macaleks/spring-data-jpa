a5f9bed31134bba43856d6b187fc033ee69ec60c
Aleksander <msxander@gmail.com>
2021-02-18 09:50:36 +0300

    Revert a change

--- src/main/java/ru/otus/jdbcprj/dao/AuthorRepository.java
+++ src/main/java/ru/otus/jdbcprj/dao/AuthorRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.otus.jdbcprj.model.Author;
 
+@Repository
 public interface AuthorRepository extends JpaRepository<Author, Long> {
 }
